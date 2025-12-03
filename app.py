0b55a89630603a25f24be96e3331815cb3f35298
lauurenlr <lauuurenlr@gmail.com>
2025-12-03 15:24:32 -0500

    Update app.py

--- app.py
+++ app.py
@@ -125,7 +125,8 @@ def update_graph(values_country, range_year, range_emision):
       x = "Year",
       y = "CO2 emission (Tons)",
       title= f"CO2 Emissions in {values_country}",
-      height=600
+      height=600,
+      width=800
   )
 
   # ---------------------- GRAPH 2: CAKE GRAPH ----------------------
@@ -134,7 +135,8 @@ def update_graph(values_country, range_year, range_emision):
       names = "Year",
       values = "CO2 emission (Tons)",
       title= f"Percentage of CO2 Emissions per year in {values_country}",
-      height=600
+      height=600,
+      width=800
   )
 
   # ---------------------- GRAPH 2: LINE GRAPH ----------------------
@@ -143,7 +145,8 @@ def update_graph(values_country, range_year, range_emision):
       x = "Year",
       y = "CO2 emission (Tons)",
       title= f"Evolution of CO2 Emissions in {values_country}" ,
-      height=600
+      height=600,
+      width=800
   )
   line_graph.update_traces(
       line=dict( 
